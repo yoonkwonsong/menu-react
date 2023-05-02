6647f86a7c7fe324a2a9f14bbe34e4709dc56a43
yoonkwonsong <78392347+yoonkwonsong@users.noreply.github.com>
2023-05-02 18:09:46 +0900

    c

--- menu-react/src/App.js
+++ menu-react/src/App.js
@@ -1,5 +1,38 @@
+import React from "react"
+import Menu from "./Menu"
+import Categories from "./Categories"
+import items from "./data"
+
 function App() {
-    return <div className="App"></div>
+    const [menuItems, setMenuItems] = React.useState(items)
+    // eslint-disable-next-line
+    const [categories, setCategories] = React.useState([
+        "all",
+        ...new Set(items.map((item) => item.category)),
+    ])
+
+    const filterItems = (category) => {
+        if (category === "all") {
+            setMenuItems(items)
+            return
+        }
+
+        const newItems = items.filter((item) => item.category === category)
+        setMenuItems(newItems)
+    }
+
+    return (
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>our menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories categories={categories} filterItems={filterItems} />
+                <Menu items={menuItems} />
+            </section>
+        </main>
+    )
 }
 
 export default App
